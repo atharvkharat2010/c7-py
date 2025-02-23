915f43f543380306f069d0fe0206034945f87360
atharvkharat2010 <atharvabk1999@gmail.com>
2025-02-23 21:14:06 +0530

    Add files via upload

---
+++ intersection.py
@@ -0,0 +1,8 @@
+setx={"green","blue"}
+sety={"blue","yellow"}
+print("original sets")
+print(setx)
+print(sety)
+print("\nIntersection of two sets")
+setz=setx.intersection(sety)
+print(setz)
---
+++ tuple.py
@@ -0,0 +1,17 @@
+my_tuple=()
+print(my_tuple)
+my_tuple=(1,2,3,)
+print(my_tuple)
+my_tuple=(1,"hello",3.4)
+print(my_tuple)
+my_tuple=('p','e','r','m','i','t')
+print(my_tuple[0])
+print(my_tuple[5])
+#nested tuple
+n_tuple=("mouse",[2,4,3],[6,0,8])
+print(n_tuple)
+print(n_tuple[0][2])
+print(n_tuple[2][2])
+print("sliced",my_tuple[1:4])
+for letter in (my_tuple):
+    print("hello",letter)
